51ea48ca6217491640e7d6eee47ca7fe92c776bd
sgeuns <jk04140@gmail.com>
2022-05-29 05:54:56 +0900

    시간초과라서, 새롭게 구현. - python의 내장함수이 list에 대한 인덱스찾기,특정값존재여부를 활용

--- 1_1 신고결과받기.py
+++ 1_1 신고결과받기.py
@@ -10,35 +10,24 @@ def solution(id_list, report, k):
 
         who = (report[x].split())[0]
         to = (report[x].split())[1]
-        print('---------'+who+'가 '+'--> ['+to+']'+'를 신고!'+'---------')
-        print()
 
         #to의 인덱스번호 찾기
-        for x in range(len(id_list)):
-            if(id_list[x] == to):
-                to_index = x
+        to_index = id_list.index(to)
 
         #이미 동일인을 신고한 이력이있는지
-        iss = 0
-        for x in answer2[to_index]:
-            if(x == who):
-                iss = 1
-
-        if(iss == 0):
+        if who not in answer2[to_index]:
             answer2[to_index].append(who)
 
-        print("[ muzi | frodo | apeach | neo ]")
-        print(answer2)
-        print()
+
 
     for t in answer2:
         if(len(t) >= k):
             for y in range(len(t)):
                 #t[y]  이 이름의 인덱스번호 찾기
-                for x in range(len(id_list)):
-                    if(id_list[x] == t[y]):
-                        who_index = x
+                who_index = id_list.index(t[y])
                 answer[who_index] += 1
+
+
     return answer
 
 
